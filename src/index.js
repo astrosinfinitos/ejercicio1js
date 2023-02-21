dd2ee70f7cd6c62b9d2368533b48016b9df84f8c
astrosinifinitos <stroncio1981@gmail.com>
2023-02-21 18:21:09 +0100

    Ejercicio del dado cambiado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+Math.floor(Math.random() * 6 ) + 1;
 
+class Dice{
+    constructor (nCaras){
+        this.caras = nCaras;
+    }
+    rollDice (){
+        return Math.floor(Math.random() * this.caras) +1;
+    }
+}
+
+const dado1 = new Dice(6);
+const dado2 = new Dice(12);
+
+console.log(dado1.rollDice())
+console.log(dado2.rollDice())
